bcb3741ed9f299d72d90628b78704180e1c5a3ea
Mostakin <mostakin4556@gmail.com>
2025-10-01 20:32:27 +0600

    update

--- src/components/Home/scroller.jsx
+++ src/components/Home/scroller.jsx
@@ -18,7 +18,11 @@ const AutoScrollingText = () => {
       {/* Inject the animation CSS directly */}
       <style>{marqueeStyle}</style>
 
-      <div className="max-w-auto ml-4 mr-4 lg:max-w-[1370px] lg:ml-4 lg:mr-4 xl:w-full xl:ml-56 mx-auto overflow-hidden bg-gradient-to-r from-blue-100 via-blue-200 to-blue-100 py-2 px-4 shadow-md rounded-md  backdrop-blur-sm bg-opacity-80">
+      <div
+        className="lg:mx-auto lg:max-w-[1370px] mx-4
+        overflow-hidden bg-gradient-to-r from-blue-100 via-blue-200 to-blue-100 py-2 px-4
+       shadow-md rounded-md  backdrop-blur-sm bg-opacity-80"
+      >
         <div
           className="flex items-center animate-marquee whitespace-nowrap text-blue-700 font-semibold text-sm sm:text-base tracking-wide"
           style={{
