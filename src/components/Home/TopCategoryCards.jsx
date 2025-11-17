65839c73f01422ad5975264517158ff6fb7c1968
Mostakin <mostakin4556@gmail.com>
2025-11-17 19:49:52 +0600

    work on home

--- src/components/Home/TopCategoryCards.jsx
+++ src/components/Home/TopCategoryCards.jsx
@@ -9,13 +9,14 @@ const TopCategoryCards = () => {
   return (
     <div className="max-w-[1400px] mx-auto mb-7">
       <FeatureText data="Top Categories" />
-      <div className="grid grid-cols-4 md:grid-cols-7 lg:grid-cols-8 xl:grid-cols-10 gap-2 xl:gap-7 mx-2 lg:mx-4 xl:mr-5">
+      <div className="grid grid-cols-4 md:grid-cols-7 lg:grid-cols-8 xl:grid-cols-10 gap-3 xl:gap-7 mx-2 lg:mx-4 xl:mr-5">
         {categoryData && categoryData.length > 0 ? (
           categoryData.map((cat, index) => (
             <Link to={`/product/${cat.catID}`}>
               <div
                 key={index}
-                className="bg-white shadow-md rounded-lg flex flex-col items-center justify-center h-22 w-23   lg:w-29 lg:h-28 text-center hover:scale-105 hover:text-[#fe741d] transform transition duration-300 cursor-pointer"
+                className="bg-white shadow-md rounded-lg flex flex-col items-center justify-center h-21 w-22 lg:w-29 lg:h-28 
+                text-center hover:scale-105 hover:text-[#fe741d] transform transition duration-300 cursor-pointer"
               >
                 {/* Icon: use cat.icon if available, otherwise default */}
                 {cat.icon ? (
