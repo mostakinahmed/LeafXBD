cd70351da16d8a6d433f4eb66c8f5303abb7a28a
Mostakin <mostakin4556@gmail.com>
2025-11-23 17:10:59 +0600

    update product card

--- src/components/Home/FlashSale.jsx
+++ src/components/Home/FlashSale.jsx
@@ -1,5 +1,5 @@
 import { img } from "framer-motion/client";
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { FiZap } from "react-icons/fi"; // Feather icon
 import { DataContext } from "../Context Api/UserContext";
 import { Link } from "react-router-dom";
@@ -7,14 +7,12 @@ import ProductCard from "../ProductCard";
 
 export default function FlashSale() {
   const { productData } = useContext(DataContext);
+  const [flashData, setFlashData] = useState([]);
 
-  const products = new Array(6).fill(0).map((_, i) => ({
-    id: i + 1,
-    title: `Product ${i + 1}`,
-    price: (99 + i * 25).toFixed(2),
-    img: `https://www.mobilebd.co/wp-content/uploads/2025/11/Samsung-Galaxy-S29-FE-Navy-Image-300x300.png`,
-    // img: `https://via.placeholder.com/400x300?text=Product+${i + 1}`,
-  }));
+  useEffect(() => {
+    const data = productData.filter((item) => item.status.isFlashSale);
+    setFlashData(data);
+  }, [productData]);
 
   return (
     <section className=" max-w-[1370px] bg-gray-800 pb-3 xl:mx-auto mx-2 lg:mx-4 rounded">
@@ -37,17 +35,14 @@ export default function FlashSale() {
       </div>
 
       <div className="max-w-[1400px] mx-auto lg:px-4 px-2 pb-4 grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-6 md:gap-4 gap-3">
-        {productData
-          .reverse()
-          .slice(0, 6)
-          .map((product) => (
-            <Link
-              key={product.pID}
-              to={`/product/${product.category}/${product.pID}`}
-            >
-              <ProductCard data={product} />
-            </Link>
-          ))}
+        {flashData.slice(0, 6).map((product) => (
+          <Link
+            key={product.pID}
+            to={`/product/${product.category}/${product.pID}`}
+          >
+            <ProductCard data={product} />
+          </Link>
+        ))}
       </div>
     </section>
   );
