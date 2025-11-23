3aca79b12711ff2d9efac1a33eb263ac4218203f
Mostakin <mostakin4556@gmail.com>
2025-11-23 17:14:36 +0600

    update product card

--- src/components/Home/FeatureProduct.jsx
+++ src/components/Home/FeatureProduct.jsx
@@ -6,8 +6,14 @@ import { Link } from "react-router-dom";
 
 export const FeatureProduct = () => {
   const { productData } = useContext(DataContext);
-
   const [visibleProducts, setVisibleProducts] = useState([]);
+
+  const [featuredData, setFeaturedData] = useState([]);
+
+  useEffect(() => {
+    const data = productData.filter((item) => item.status.isFeatured);
+    setFeaturedData(data);
+  }, [productData]);
 
   useEffect(() => {
     const updateVisibleProducts = () => {
@@ -15,13 +21,13 @@ export const FeatureProduct = () => {
 
       if (width >= 1280) {
         // xl screens
-        setVisibleProducts(productData.slice(0, 16));
+        setVisibleProducts(featuredData.slice(0, 16));
       } else if (width >= 1024) {
         // lg screens
-        setVisibleProducts(productData.slice(0, 12));
+        setVisibleProducts(featuredData.slice(0, 12));
       } else {
         // md/sm/xs
-        setVisibleProducts(productData.slice(0, 8));
+        setVisibleProducts(featuredData.slice(0, 8));
       }
     };
 
@@ -38,14 +44,17 @@ export const FeatureProduct = () => {
       <FeatureText data="Featured Product" />
 
       <div className="max-w-[1400px] mx-auto lg:px-4 px-2 pb-4 grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-6 xl:grid-cols-6 md:gap-4 gap-3">
-        {visibleProducts.slice(0,12).reverse().map((product) => (
-          <Link
-            key={product.pID}
-            to={`/product/${product.category}/${product.pID}`}
-          >
-            <ProductCard data={product} />
-          </Link>
-        ))}
+        {visibleProducts
+          .slice(0, 12)
+          .reverse()
+          .map((product) => (
+            <Link
+              key={product.pID}
+              to={`/product/${product.category}/${product.pID}`}
+            >
+              <ProductCard data={product} />
+            </Link>
+          ))}
       </div>
     </div>
   );
