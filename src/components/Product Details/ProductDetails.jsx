b5b371592cc125bc2bd627f4e61f124007fc455a
Mostakin <mostakin4556@gmail.com>
2025-12-04 20:55:17 +0600

    update

--- src/components/Product Details/ProductDetails.jsx
+++ src/components/Product Details/ProductDetails.jsx
@@ -302,9 +302,7 @@ const ProductDetail = () => {
           </div>
         </section>
 
- 
-
-        {/* Specs and Related */}
+ }
         {/* Specs and Related */}
         <section className="max-w-full lg:max-w-[1400px] mx-auto md:px-5 px-2 flex flex-col lg:flex-row lg:gap-3">
           <div className="lg:w-full xl:w-full">
